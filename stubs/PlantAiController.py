46cee700e807cf041e8a4fbc00e3fa9994055945
alemau <33016970+alemaulou@users.noreply.github.com>
2019-10-29 13:40:09 -0700

    Update PlantAiController.py

--- stubs/PlantAiController.py
+++ stubs/PlantAiController.py
@@ -1,1 +1,11 @@
+# Pre-conditions: Retrieves image from the request.
+# Post-conditions: Changes format for PlantAIModel and runs PlantAIModel on image for classification. Returns output as HTTP Response.
 
+import modelfile 
+import json 
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+
+def predict_disease(request):
+  return HttpResponse(json(data))
