0f2e6e2877094d24a952643c8a05b0926123c27f
mickey <mikias@uw.edu>
2019-11-12 17:12:19 -0800

    Frontend V1

--- PlantPharmacy/frontend/src/components/Layout/WelcomeView.js
+++ PlantPharmacy/frontend/src/components/Layout/WelcomeView.js
@@ -1,10 +1,29 @@
-import React from 'react';
-
-// static welcome page content
-const WelcomeView = () => (
- <div>
-
- </div>
-);
-
+import React, { Component } from "react";
+class WelcomeView  extends Component {
+    render() { 
+        return (  
+            <div>
+                <section className ="welcomeSection">
+                    <div className="mainContainer container">
+                        <div className="d-flex flex-column justify-content-between welcomeWrapper">
+                            <div className="welcomeDetails">
+                                <h3>At <span className="heroHeader">PlantPharmacy</span>,<br/>we help save your plants.</h3>
+                                <p>Upload an image of your ailing plant and we'll let you know
+                                the disease detected, allowing you to treat your plants more confidently.</p>
+                                <button className="uploadBtn" type="button">Upload Image</button>
+                                <img/>
+                            </div>  
+                            <p className="uploadCaption">
+                            For best results, upload an image (JPG or PNG)
+                            that’s close to the subject and between 1MB and 10MB in size.
+                            </p>
+                        </div>
+                        
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+ 
 export default WelcomeView;
