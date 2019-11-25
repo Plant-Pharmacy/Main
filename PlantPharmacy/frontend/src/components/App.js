638b6ead0f5e964d789d0f5a14a641b2f1ae1114
kyrabautista <kyrabb@uw.edu>
2019-11-25 12:24:14 -0800

    error messages

--- PlantPharmacy/frontend/src/components/App.js
+++ PlantPharmacy/frontend/src/components/App.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import HeaderView from "./Layout/HeaderView"
-import WelcomeView from "./Layout/WelcomeView"
-import ImageView from "./Layout/ImageView"
-import ResultsView from "./Layout/ResultsView"
-import FooterView from "./Layout/FooterView"
+import HeaderView from "./Layout/HeaderView";
+import WelcomeView from "./Layout/WelcomeView";
+import ImageView from "./Layout/ImageView";
+import ResultsView from "./Layout/ResultsView";
+import FooterView from "./Layout/FooterView";
+import ErrorView from "./Layout/ErrorView";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "../css/App.css";
 
 class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <HeaderView/>
-                <WelcomeView/>
-                <section className ="resultsSection">
-                    <ImageView/>
-                    <ResultsView/>
-                </section>              
-
-
-                <FooterView/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <HeaderView />
+        <WelcomeView />
+        <section className="resultsSection">
+          <ImageView />
+          <ResultsView />
+        </section>
+        <ErrorView />
+        <FooterView />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
