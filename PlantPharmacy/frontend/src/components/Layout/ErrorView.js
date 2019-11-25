220ff807569efd411b6d31f231b01cfdf4a29d2e
kyrabautista <kyrabb@uw.edu>
2019-11-25 12:24:14 -0800

    error messages

--- PlantPharmacy/frontend/src/components/Layout/ErrorView.js
+++ PlantPharmacy/frontend/src/components/Layout/ErrorView.js
@@ -1,22 +1,163 @@
-import React, {Component} from 'react';
+// import React, { Component } from "react";
+// import { PopupboxManager, PopupboxContainer } from "react-popupbox";
 
+// export class ErrorView extends Component {
+//   openPopupbox() {
+//     const content = (
+//       <div>
+//         <p>
+//           Please upload an image with a file that is no less than 1440x1080
+//           pixels, 3.1 MP and no more than 4032x3024 pixels, 10 MP
+//         </p>
+//       </div>
+//     );
+//     PopupboxManager.open({ content });
+//   }
+//   openPopupboxImageSize() {
+//     const content = (
+//       <div>
+//         <p>
+//           message 2: Please upload an image with a file that is no less than
+//           1440x1080 pixels, 3.1 MP and no more than 4032x3024 pixels, 10 MP
+//         </p>
+//       </div>
+//     );
+//     PopupboxManager.open({ content });
+//   }
 
-// Displays error to user in dialog
+//   render() {
+//     const popupboxConfig = {
+//       titleBar: {
+//         enable: true,
+//         text: "Error: Invalid file size"
+//       },
+//       fadeIn: true,
+//       fadeInSpeed: 500
+//     };
+//     const popupboxConfig2 = {
+//       titleBar: {
+//         enable: true,
+//         text: "Error2: Invalid file size"
+//       },
+//       fadeIn: true,
+//       fadeInSpeed: 500
+//     };
+//     return (
+//       <div>
+//         <button onClick={this.openPopupboxImageSize}>Click me</button>
+//         <PopupboxContainer {...popupboxConfig2} />
+//         {console.log(popupboxConfig.titleBar.text)}
+//       </div>
+//     );
+//   }
+// }
 
-// Required props: errorMessage
-class ErrorView extends Component {
-    constructor(props) {
-        super(props)
-    }
+// import React, { Component } from "react";
+// import { Button, Modal } from "react-bootstrap";
 
-    // displays image
-    render() {
-        return (
-            <div>
-                {this.props.errorMessage}
-            </div>
-        )
-    }
+// // Displays error to user in dialog
+
+// // Required props: errorMessage
+
+// class ErrorView extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       show: false
+//     };
+//   }
+
+//   // Handles Error Modal toggle
+//   handleModal() {
+//     this.setState({ show: !this.state.show });
+//   }
+//   // Renders Error modal
+//   render() {
+//     return (
+//       <div>
+//         <Button
+//           onClick={() => {
+//             this.handleModal();
+//           }}
+//         >
+//           Test
+//         </Button>
+//         <Modal
+//           className="myError"
+//           id="imageSizeError"
+//           show={this.state.show}
+//           onHide={() => this.handleModal()}
+//           size="sm"
+//           aria-labelledby="contained-modal-title-vcenter"
+//           centered
+//         >
+//           <Modal.Header closeButton>Error: Invalid file size</Modal.Header>
+//           <Modal.Body>
+//             <p>
+//               Please upload an image with a file that is no less than 1440x1080
+//               pixels, 3.1 MP and no more than 4032x3024 pixels, 10 MP
+//             </p>
+//           </Modal.Body>
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+//testing out functions
+import React, { Component } from "react";
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+
+export class ErrorView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
+  //   Error variables
+  errorTitleImageSize = "Error: Invalid file size";
+  errorMessageImageSize =
+    "Please upload an image with a file that is no less than 1440x1080pixels, 3.1 MP and no more than 4032x3024 pixels, 10 MP";
+  errorTitleFileType = "Error: Invalid file type";
+  errorMessageFileType = "Please upload either a .JPG or .PNG image";
+
+  // Error title
+  popupboxConfig = {
+    titleBar: {
+      enable: true,
+      text: "Error"
+    },
+    fadeIn: true,
+    fadeInSpeed: 500
+  };
+  // Error message container
+  openPopupbox() {
+    const content = (
+      <div>
+        <p></p>
+      </div>
+    );
+    PopupboxManager.open({ content });
+  }
+  // Handles Error Modal
+  handleErrorMessage(message) {
+    this.openPopupbox.content = message;
+    console.log("this is openPopupbox");
+    console.log(this.openPopupbox);
+    return this.openPopupbox;
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleErrorMessage(this.errorMessageFileType)}>
+          Click me
+        </button>
+        <PopupboxContainer {...this.popupboxConfig} />
+      </div>
+    );
+  }
 }
 
-export default ImageView;
+export default ErrorView;
--- PlantPharmacy/frontend/src/components/Layout/ImageView.js
+++ PlantPharmacy/frontend/src/components/Layout/ImageView.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // Displays image back to user
 
 // Required props: image
 class ImageView extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    // displays image
-    render() {
-        return (
-            <div>
-                    <div className="mainContainer container">
-                        <div className="imageViewSection">
-                            <div className="titleLine"></div>
-                            <h4>Your plant report</h4>
-                        </div>
-                    </div>
-            </div>
-        )
-    }
+  // displays image
+  render() {
+    return (
+      <div>
+        <div className="mainContainer container">
+          <div className="imageViewSection">
+            <div className="titleLine"></div>
+            <h4>Your plant report</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ImageView;
